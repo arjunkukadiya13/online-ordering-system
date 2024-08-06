7280b11f46f56100c2c6447d6726bd2e00ef97bc
KUKADIYA ARJUN <arjunkukadiya58@gmail.com>
2024-08-06 17:37:59 +0530

    Header and Footer created

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="feedback-bar">
+      <span>Help us make Smart Queue better! 😃</span>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <div className="menu-icon">&#9776; Menu</div>
+      <div className="search-icon">&#128269;</div>
+    </div>
+  );
+};
+
+export default Header;
